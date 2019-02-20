7902c67b3ff8bc8005b2f50744445dcafec9f5b1
Madeline Hanley <mehanley2@gmail.com>
2019-02-20 12:47:25 -0500

    created config file for dynamic content generation

--- tech-train/src/Sidebar/Sidebar.js
+++ tech-train/src/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Sidebar.css';
-//import config from 'config';
+import config from 'config';
 
 function Sidebar (props) {
     console.log(props);
@@ -8,13 +8,7 @@ function Sidebar (props) {
     return (
             <div className='sidebar'>
             <ul className='sidebar-list'> 
-                <li className={`menu-item ${props.topic === 'ES6' ? 'active' : ''}`}><div id='ES6' onClick={props.changeTopic} >ES6</div></li>
-                <li className={`menu-item ${props.topic === 'ReactJS' ? 'active' : ''}`}><div id='ReactJS' onClick={props.changeTopic} >ReactJS</div></li>
-                <li className={`menu-item ${props.topic === 'Redux' ? 'active' : ''}`}><div id='Redux' onClick={props.changeTopic} >Redux</div></li>
-                <li className={`menu-item ${props.topic === 'CSS' ? 'active' : ''}`}><div id='CSS' onClick={props.changeTopic} >CSS</div></li>
-                <li className={`menu-item ${props.topic === 'EsriJSAPI' ? 'active' : ''}`}><div id='EsriJSAPI' onClick={props.changeTopic} >EsriJSAPI</div></li>
-                <li className={`menu-item ${props.topic === 'Node.js' ? 'active' : ''}`}><div id='Node.js' onClick={props.changeTopic} >Node.js</div></li>
-                
+            <li className={`menu-item ${props.topic === 'ES6' ? 'active' : ''}`}><div id='ES6' onClick={props.changeTopic} >ES6</div></li>        
             </ul>
             </div>
 
---
+++ tech-train/src/config/config.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const config = {
+    appTitle: 'Tech Train 💪💻',
+
+    subjects: [
+        {
+            label: 'ES6',
+            info: <div>ES6 is a new version of JavaScript. We use it a lot in React</div>
+        },
+        {
+            label: 'ReactJS',
+            info: <div>React is a front-end JavaScript framework developed by Facebook</div>
+        },
+        {
+            label: 'Redux',
+            info: <div>The state of things.</div>
+        },
+        {
+            label: 'CSS',
+            info: <div>The most stylish language</div>
+        },
+        {
+            label: 'EsriJSAPI',
+            info: <div>Our map API</div>
+        },
+        {
+            label: 'NodeJS',
+            info: <div>JavaScript for the back-end</div>
+        },
+]
+}
